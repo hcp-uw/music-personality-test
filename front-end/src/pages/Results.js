7a01655922b1a5d1e03a07861b7f7daf6dc029b1
Enrico <enricopratama10@gmail.com>
2023-07-11 16:37:11 +0700

    Added descriptions

--- front-end/src/pages/Results.js
+++ front-end/src/pages/Results.js
@@ -17,9 +17,18 @@ export default function ResultsMain() {
             </div>
             
             <div className='rectangle-1'>
+                <p className='rectangle-title1'>What your music personality tells you:</p>
                 <p className='rectangle-desc1'>
                     You are one who prefers listening to X type of music rather than other types such as Y and Z types. 
                 </p>
+            </div>
+
+            <div className='rectangle-2'>
+                <p className='rectangle-title2'>Music Genre Recommendations</p>
+                <p className='rectangle-desc2'>
+                Based on your answers, our algorithm suggests that you listen to pop, R&B, and soul as you are more of on the calmer side.
+                </p>
+                
             </div>
         </main>
         
