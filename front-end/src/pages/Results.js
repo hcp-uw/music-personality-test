1d1a8fc5746cf5a3a738c0766193e7a3b9037092
Enrico <enricopratama10@gmail.com>
2023-08-18 22:42:05 +0700

    responsive results page

--- front-end/src/pages/Results.js
+++ front-end/src/pages/Results.js
@@ -3,34 +3,32 @@ import planetImage from "../images/planet-02.png"
 
 export default function ResultsMain() {
     return (
-        <main>
-            <div className='results-box'>
-                <div className='results-main'>
+    <main>
+    <div className='overall'>
+        <div className='overall-container'>
+            <div className='results-container'>
+                <div className='results-box'>
                     <img src={planetImage} alt="resultPlanetPic" className="result-image" />
+                    <h1>Result: <br></br> The Voyager!</h1>
                 </div>
-                <p className='results-desc-title'>
-                    Your results:
-                </p>
-                <p className='results-desc'>
-                    The Voyager!
-                </p>
+                <div className='right-section'>
+                    <div className='results-box2'>
+                        <h1 className='text-results'>What your music personality tells you:</h1>
+                        <p className='text-results'>You are one who prefers listening to X type of music rather than other types such as Y and Z types.</p>
+                    </div>
+                    <div className='results-box3'>
+                        <h1 className='text-results'>What your music personality tells you:</h1>
+                        <p className='text-results'>You are one who prefers listening to X type of music rather than other types such as Y and Z types.</p>
+                    </div>
+                </div>
             </div>
-            
-            <div className='rectangle-1'>
-                <p className='rectangle-title1'>What your music personality tells you:</p>
-                <p className='rectangle-desc1'>
-                    You are one who prefers listening to X type of music rather than other types such as Y and Z types. 
-                </p>
+        </div>
+       <div className='button1'>
+        <div className='results-button'>
+                <p className='text-results'>Take another test here!</p>
             </div>
-
-            <div className='rectangle-2'>
-                <p className='rectangle-title2'>Music Genre Recommendations</p>
-                <p className='rectangle-desc2'>
-                Based on your answers, our algorithm suggests that you listen to pop, R&B, and soul as you are more of on the calmer side.
-                </p>
-                
-            </div>
-        </main>
-        
+       </div>
+    </div>
+    </main>
     )
 }
