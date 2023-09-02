476fdb9b084b87a5c6254fe5b8ac4be04161a761
Sam Shin <samshin0714@gmail.com>
2023-09-02 01:12:22 -0700

    Backend

--- front-end/backend/app.js
+++ front-end/backend/app.js
@@ -9,11 +9,168 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 const API_KEY = "ABC";
+const QUESTION = [
+    {
+        id: 1,
+        question: "Question 1",
+    },
+    {
+        id: 2,
+        question: "Question 2",
+    },
+    {
+        id: 3,
+        question: "Question 3",
+    },
+    {
+        id: 4,
+        question: "Question 4",
+    },
+    {
+        id: 5,
+        question: "Question 5",
+    },
+    {
+        id: 6,
+        question: "Question 6",
+    },
+    {
+        id: 7,
+        question: "Question 7",
+    },
+    {
+        id: 8,
+        question: "Question 8",
+    },
+    {
+        id: 9,
+        question: "Question 9",
+    },
+    {
+        id: 10,
+        question: "Question 10",
+    },
+    {
+        id: 11,
+        question: "Question 11",
+    },
+    {
+        id: 12,
+        question: "Question 12",
+    },
+    {
+        id: 13,
+        question: "Question 13",
+    },
+    {
+        id: 14,
+        question: "Question 14",
+    },
+    {
+        id: 15,
+        question: "Question 15",
+    },
+    {
+        id: 16,
+        question: "Question 16",
+    },
+    {
+        id: 17,
+        question: "Question 17",
+    },
+    {
+        id: 18,
+        question: "Question 18",
+    },
+    {
+        id: 19,
+        question: "Question 19",
+    },
+    {
+        id: 20,
+        question: "Question 20",
+    },
+    {
+        id: 21,
+        question: "Question 21",
+    },
+    {
+        id: 22,
+        question: "Question 22",
+    },
+    {
+        id: 23,
+        question: "Question 23",
+    },
+    {
+        id: 24,
+        question: "Question 24",
+    },
+    {
+        id: 25,
+        question: "Question 25",
+    },
+    {
+        id: 26,
+        question: "Question 26",
+    },
+    {
+        id: 27,
+        question: "Question 27",
+    },
+    {
+        id: 28,
+        question: "Question 28",
+    },
+    {
+        id: 29,
+        question: "Question 29",
+    },
+    {
+        id: 30,
+        question: "Question 30",
+    },
+    {
+        id: 31,
+        question: "Question 31",
+    },
+    {
+        id: 32,
+        question: "Question 32",
+    },
+    {
+        id: 33,
+        question: "Question 33",
+    },
+    {
+        id: 34,
+        question: "Question 34",
+    },
+    {
+        id: 35,
+        question: "Question 35",
+    },
+    {
+        id: 36,
+        question: "Question 36",
+    },
+]
 
 app.get('/', (req, res) => {
     let resp = "The server is set up correctly";
     res.status(200).send(resp);
 });
+
+app.get('/questions', (req, res) => {
+    let output = [...QUESTION];
+
+    for (let i = output.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [output[i], output[j]] = [output[j], output[i]];
+    }
+
+    res.json(output);
+})
 
 // FIXME: Currently saving plain_password just to debug. REMOVE LATER
 app.post('/register', async (req, res) => {
